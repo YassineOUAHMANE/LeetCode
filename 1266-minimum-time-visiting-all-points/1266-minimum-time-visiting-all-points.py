5ced1fa1188a2354d2cdd5d3e5fc92758690c6d5
YASSINE OUAHMANE <144154410+YassineOUAHMANE@users.noreply.github.com>
2024-10-29 21:58:52 +0100

    Time: 0 ms (100.00%), Space: 16.8 MB (10.61%) - LeetHub

--- 1266-minimum-time-visiting-all-points/1266-minimum-time-visiting-all-points.py
+++ 1266-minimum-time-visiting-all-points/1266-minimum-time-visiting-all-points.py
@@ -1,42 +1,73 @@
 class Solution:
     def minTimeToVisitAllPoints(self, points: List[List[int]]) -> int:
-        count_secondes = 0 
-        for i in range(len(points)-1):
-            x = points[i][0]
-            y = points[i][1]
-            while x!=points[i+1][0] and y!=points[i+1][1]:
-                if x > points[i+1][0] and y > points[i+1][1]:
-                    x-=1
-                    y-=1
-                    count_secondes+=1
-                if x < points[i+1][0] and y > points[i+1][1]: 
-                    x+=1
-                    y-=1
-                    count_secondes+=1
-                    
-                if x > points[i+1][0] and y < points[i+1][1]:
-                    y+=1
-                    x-=1
-                    count_secondes+=1
-                if x < points[i+1][0] and y < points[i+1][1]:
-                    x+=1
-                    y+=1
-                    count_secondes+=1
-            while y!=points[i+1][1]:        
-                if x==points[i+1][0] and y > points[i+1][1]:
-                    y-=1
-                    count_secondes+=1
-                if x==points[i+1][0] and y < points[i+1][1]:
-                    y+=1
-                    count_secondes+=1
-            while x!=points[i+1][0]:        
-                if x > points[i+1][0] and y==points[i+1][1]:
-                    x-=1 
-                    count_secondes+=1
-                if x < points[i+1][0] and y==points[i+1][1]:  
-                    x+=1
-                    count_secondes+=1
-        return count_secondes              
+        count_secondes = 0
+        x1,y1 = points.pop()
+        while points:
+            x2,y2 = points.pop()
+            count_secondes += max(abs(y2-y1),abs(x2-x1))
+            x1,y1 = x2,y2
+        return count_secondes    
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        #count_secondes = 0 
+        #for i in range(len(points)-1):
+        #    x = points[i][0]
+        #    y = points[i][1]
+        #    while x!=points[i+1][0] and y!=points[i+1][1]:
+        #        if x > points[i+1][0] and y > points[i+1][1]:
+        #            x-=1
+        #            y-=1
+        #            count_secondes+=1
+        #        if x < points[i+1][0] and y > points[i+1][1]: 
+        #            x+=1
+        #            y-=1
+        #            count_secondes+=1
+        #            
+        #        if x > points[i+1][0] and y < points[i+1][1]:
+        #            y+=1
+        #            x-=1
+        #            count_secondes+=1
+        #        if x < points[i+1][0] and y < points[i+1][1]:
+        #            x+=1
+        #            y+=1
+        #            count_secondes+=1
+        #    while y!=points[i+1][1]:        
+        #        if x==points[i+1][0] and y > points[i+1][1]:
+        #            y-=1
+        #            count_secondes+=1
+        #        if x==points[i+1][0] and y < points[i+1][1]:
+        #            y+=1
+        #            count_secondes+=1
+        #    while x!=points[i+1][0]:        
+        #        if x > points[i+1][0] and y==points[i+1][1]:
+        #            x-=1 
+        #            count_secondes+=1
+        #        if x < points[i+1][0] and y==points[i+1][1]:  
+        #            x+=1
+        #            count_secondes+=1
+        #return count_secondes
+        
+        
+        
             
         
         
