137b70007cb362e765b8a3dc7958b29d9da2df14
YASSINE OUAHMANE <144154410+YassineOUAHMANE@users.noreply.github.com>
2024-11-03 20:19:31 +0100

    Time: 86 ms (28.09%), Space: 27.2 MB (72.60%) - LeetHub

--- 0160-intersection-of-two-linked-lists/0160-intersection-of-two-linked-lists.py
+++ 0160-intersection-of-two-linked-lists/0160-intersection-of-two-linked-lists.py
@@ -6,16 +6,44 @@
 
 class Solution:
     def getIntersectionNode(self, headA: ListNode, headB: ListNode) -> Optional[ListNode]:
-        stock_memory_nodes = set()
-        while headA:
-            stock_memory_nodes.add(headA)
+        
+        # This is O(n) time and space complexity
+        #stock_memory_nodes = set()
+        #while headA:
+        #    stock_memory_nodes.add(headA)
+        #    headA = headA.next
+        #while headB:
+        #    if headB in stock_memory_nodes:
+        #        return headB
+        #    else:
+        #        headB = headB.next
+        #return   
+        
+        #what about O(1) space complexity ?
+        def get_length(head):
+            counter = 0
+            while head:
+                counter+=1
+                head = head.next
+            return counter
+        
+        lenA = get_length(headA)
+        lenB = get_length(headB)
+        
+        while (lenA>lenB):
+            lenA-=1
             headA = headA.next
-        while headB:
-            if headB in stock_memory_nodes:
-                return headB
-            else:
-                headB = headB.next
-        return        
+        while (lenB>lenA):
+            lenB-=1
+            headB = headB.next
+        
+        while headA!=headB:
+            headA = headA.next
+            headB = headB.next
+            if not headA or not headB:
+                return
+        return headA
+    
             
                 
         
