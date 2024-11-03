2e797225839cb8c81ad38e40e455675fdacd0cab
YASSINE OUAHMANE <144154410+YassineOUAHMANE@users.noreply.github.com>
2024-11-03 21:57:16 +0100

    Time: 79 ms (25.73%), Space: 17.1 MB (80.86%) - LeetHub

--- 2405-optimal-partition-of-string/2405-optimal-partition-of-string.py
+++ 2405-optimal-partition-of-string/2405-optimal-partition-of-string.py
@@ -2,17 +2,32 @@ class Solution:
     def partitionString(self, s: str) -> int:
         #The characters in each substring are unique 
         #No letter appears in a substring more than once
-        unique_substring = set()
+        
+#let's do another approach using a bit mask
+        bitmask = 0
         counter = 0
-        for el in s:
-            if el not in unique_substring:
-                unique_substring.add(el)
-            else:
-                counter+=1
-                unique_substring = set()
-                unique_substring.add(el)
-        counter+=1        
-        return counter        
-                
+        for ch in s:
+            bit_position = ord(ch) - ord("a")
+            
+            if bitmask & (1 << bit_position)!=0:
+                counter += 1
+                bitmask = 0
+            
+            bitmask|=(1 << bit_position)
+        return counter +  1    
+    
+        #We know that the set cannot contain more than 26 letters in alphabet so space complexity is O(1) and time complexity is O(1)
+        #unique_substring = set()
+        #counter = 0
+        #for el in s:
+        #    if el not in unique_substring:
+        #        unique_substring.add(el)
+        #    else:
+        #        counter+=1
+        #        unique_substring = set()
+        #        unique_substring.add(el)
+        #counter+=1        
+        #return counter        
+              
         
         
