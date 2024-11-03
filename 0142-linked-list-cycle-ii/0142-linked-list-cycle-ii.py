85812e1d23e80d3b018c1ee9cf60ae9d8822d8ea
YASSINE OUAHMANE <144154410+YassineOUAHMANE@users.noreply.github.com>
2024-11-03 15:38:35 +0100

    Time: 51 ms (13.84%), Space: 19.2 MB (37.55%) - LeetHub

--- 0142-linked-list-cycle-ii/0142-linked-list-cycle-ii.py
+++ 0142-linked-list-cycle-ii/0142-linked-list-cycle-ii.py
@@ -6,14 +6,46 @@
 
 class Solution:
     def detectCycle(self, head: Optional[ListNode]) -> Optional[ListNode]:
-        if not head: 
+        if not head:
+            return
+        slow,fast = head,head
+        while  fast.next and fast.next.next:
+            slow = slow.next
+            fast = fast.next.next
+            if fast == slow:
+                break
+        if not fast.next or not fast.next.next:
             return 
-        visited_nodes = []
-        while head.next:
-            if head in visited_nodes:
-                return head
-            visited_nodes.append(head)
-            head = head.next
-        return 
+        slow = head
+        while (slow!=fast):
+            fast = fast.next
+            slow = slow.next
+        return slow    
+        
+            
+            
             
         
+            
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        #This solution is O(n) space complexity and O(n) time complexity
+        #if not head: 
+        #    return 
+        #visited_nodes = {}
+        #while head.next:
+        #    if head in visited_nodes:
+        #        return head
+        #    visited_nodes.add(head)
+        #    head = head.next
+        #return 
