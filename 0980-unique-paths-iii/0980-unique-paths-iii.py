8d37fa97842c0dae227238258cee63443f55211f
YASSINE OUAHMANE <144154410+YassineOUAHMANE@users.noreply.github.com>
2024-11-03 12:22:35 +0100

    Time: 31 ms (11.24%), Space: 16.6 MB (81.72%) - LeetHub

--- 0980-unique-paths-iii/0980-unique-paths-iii.py
+++ 0980-unique-paths-iii/0980-unique-paths-iii.py
@@ -14,33 +14,23 @@ class Solution:
                     elif grid[r][c] == 0:
                         empty_cells += 1
             return start, end, empty_cells
-        
-        start, end, empty_cells = find_start_end_position_and_empty_cells(grid)
-        valid_path = [0]  # Use a list to track valid paths as a mutable count
-        
-        def dfs(current, end, remaining_cells, grid, visited_cells):
-            if current == end and remaining_cells == 0:
-                valid_path[0] += 1
+        start,end,empty_cells = find_start_end_position_and_empty_cells(grid)
+        valid_path = [0]
+        def dfs(current,end,remaining_cells,visited_cells,grid):
+            if current == end and remaining_cells == 0 :
+                valid_path[0]+=1
                 return
             
-            r, c = current
-            for dr, dc in directions:
-                nr, nc = r + dr, c + dc
-                new_position = (nr, nc)
-                
-                if (0 <= nr < len(grid) and 0 <= nc < len(grid[0]) and
-                        new_position not in visited_cells and grid[nr][nc] != -1):
-                    
+            for tup in directions:
+                r,c = tup
+                nr,nc = current[0]+r,current[1]+c
+                if 0<=nr<len(grid) and  0<=nc<len(grid[0]) and grid[nr][nc]!=-1 and (nr,nc) not in visited_cells:
+                    new_position = (nr,nc)
                     visited_cells.add(new_position)
-                    dfs(new_position, end, remaining_cells - 1, grid, visited_cells)
-                    visited_cells.remove(new_position)  # Backtrack after returning
-        
-        visited_cells = set([start])
-        dfs(start, end, empty_cells + 1, grid, visited_cells)
+                    dfs(new_position,end,remaining_cells-1,visited_cells,grid)
+                    visited_cells.remove(new_position)
+                
+        visited_cells = {start}        
+        dfs(start,end,empty_cells+1,visited_cells,grid)
         return valid_path[0]
-
                 
-                    
-                
-                
-        
