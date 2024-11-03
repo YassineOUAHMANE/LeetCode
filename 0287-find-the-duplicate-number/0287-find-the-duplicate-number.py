8d09a2c808f710746186be1e88ef35b6d6b9131c
YASSINE OUAHMANE <144154410+YassineOUAHMANE@users.noreply.github.com>
2024-11-03 16:35:20 +0100

    Time: 39 ms (30.38%), Space: 28.5 MB (96.57%) - LeetHub

--- 0287-find-the-duplicate-number/0287-find-the-duplicate-number.py
+++ 0287-find-the-duplicate-number/0287-find-the-duplicate-number.py
@@ -1,13 +1,36 @@
 class Solution:
     def findDuplicate(self, nums: List[int]) -> int:
-        seen = set()
-        for el in nums:
-            if el in seen:
-                return el 
+        #let's find a O(1) space complexity solution modifying the array
+        for i in range(len(nums)):
+            index = abs(nums[i]) - 1
+            if nums[index] < 0:
+                return abs(nums[i])
             else:
-                seen.add(el)
-                
-                            
+                nums[index] = -nums[index]
+            
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+       
+    
+    
+        #This solution is O(n)  time and  space complexity
+        #seen = set()
+        #for el in nums:
+        #    if el in seen:
+        #        return el 
+        #    else:
+        #        seen.add(el)
+                           
         
         
         
