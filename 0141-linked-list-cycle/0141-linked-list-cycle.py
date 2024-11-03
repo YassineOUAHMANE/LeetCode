9ac0ef567680d89d658ac394a6513372f61c026a
YASSINE OUAHMANE <144154410+YassineOUAHMANE@users.noreply.github.com>
2024-11-03 14:05:03 +0100

    Time: 53 ms (11.58%), Space: 19.2 MB (33.41%) - LeetHub

--- 0141-linked-list-cycle/0141-linked-list-cycle.py
+++ 0141-linked-list-cycle/0141-linked-list-cycle.py
@@ -6,19 +6,47 @@
 
 class Solution:
     def hasCycle(self, head: Optional[ListNode]) -> bool:
-        if not head:
-            return False
-        if head.next == None:
-            return False
-        visited_nodes = []
-        while head not in visited_nodes:
-            visited_nodes.append(head)
-            if head.next:
-                head = head.next
-            else:
-                return False
-                
-        return True
+        #This is O(n) time and O(1) space complexity
+        #We will use fast and slow pointer to solve this problem
+        slow,fast =head,head
+        while fast and fast.next:
+            slow = slow.next
+            fast = fast.next.next 
+            if slow == fast:
+                return True
+        return False    
+            
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        #this is O(n) time and space complexity
+        #if not head:
+        #    return False
+        #if head.next == None:
+        #    return False
+        #visited_nodes = []
+        #while head not in visited_nodes:
+        #    visited_nodes.append(head)
+        #    if head.next:
+        #        head = head.next
+        #    else:
+        #        return False      
+        #return True
         
             
             
