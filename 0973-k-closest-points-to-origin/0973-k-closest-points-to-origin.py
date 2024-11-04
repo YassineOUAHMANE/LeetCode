4e327e8cfa245f7b5249b36fb5f1872e97a85906
YASSINE OUAHMANE <144154410+YassineOUAHMANE@users.noreply.github.com>
2024-11-04 12:43:06 +0100

    Time: 51 ms (84.41%), Space: 22.3 MB (72.55%) - LeetHub

--- 0973-k-closest-points-to-origin/0973-k-closest-points-to-origin.py
+++ 0973-k-closest-points-to-origin/0973-k-closest-points-to-origin.py
@@ -3,16 +3,44 @@ from collections import defaultdict
 class Solution:
     def kClosest(self, points: List[List[int]], k: int) -> List[List[int]]:
         result = []
-        closest_points = defaultdict(list)
-        for point in points:
-            euclidien_d = sqrt(point[0]**2+point[1]**2)
-            closest_points[euclidien_d].append(point)
-        list_closest_points = sorted(closest_points.items())  
-        for i in range(k):
-            if i >= len(list_closest_points):
-                break
-            result.append(list_closest_points[i][1])
-        return sum(result,[])[:k]    
+        distances = []
+        for x,y in points:
+            distance = sqrt(x**2+y**2)
+            distances.append([distance,x,y])
+        distances.sort(key=lambda x:-x[0])
+        while k > 0:
+            result.append(distances.pop()[1:])
+            k-=1
+        return result    
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        #result = []
+        #closest_points = defaultdict(list)
+        #for point in points:
+        #    euclidien_d = sqrt(point[0]**2+point[1]**2)
+        #    closest_points[euclidien_d].append(point)
+        #list_closest_points = sorted(closest_points.items())  
+        #for i in range(k):
+        #    if i >= len(list_closest_points):
+        #        break
+        #    result.append(list_closest_points[i][1])
+        #return sum(result,[])[:k]    
             
             
         
