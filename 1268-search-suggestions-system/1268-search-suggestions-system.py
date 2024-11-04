56f4416692ea372d4a8ec743b189d5660c25c3c7
YASSINE OUAHMANE <144154410+YassineOUAHMANE@users.noreply.github.com>
2024-11-04 10:50:24 +0100

    Time: 3 ms (97.05%), Space: 19.6 MB (92.31%) - LeetHub

--- 1268-search-suggestions-system/1268-search-suggestions-system.py
+++ 1268-search-suggestions-system/1268-search-suggestions-system.py
@@ -4,19 +4,55 @@ class Solution:
         # Commom prefix
         # sorted lexicographically products
         products.sort()
+        def binary_search(List,x,start=0,end=None):
+            if end==None:
+                end = len(List)
+            while start < end:
+                mid = (start+end)//2
+                if List[mid] < x:
+                    start = mid + 1
+                else:
+                    end = mid 
+            return start
         propositions = []
-        s = ''
+        prefix = ''
         for el in searchWord:
-            s+=el
-            list_words = []
-            for word in products:
-                if word.startswith(s):
-                    list_words.append(word)
-            if len(list_words) >= 3:
-                 propositions.append(list_words[:3])
-            else:
-                 propositions.append(list_words)        
-        return propositions            
+            prefix += el
+            
+            start_position = binary_search(products,prefix)
+            suggestions = []
+            for i in range(start_position,min(start_position+3,len(products))):
+                if products[i].startswith(prefix):
+                    suggestions.append(products[i])
+                else:
+                    break
+            propositions.append(suggestions)     
+        return propositions
+            
+                    
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        #products.sort()
+        #propositions = []
+        #s = ''
+        #for el in searchWord:
+        #    s+=el
+        #    list_words = []
+        #    for word in products:
+        #        if word.startswith(s):
+        #            list_words.append(word)
+        #    if len(list_words) >= 3:
+        #         propositions.append(list_words[:3])
+        #    else:
+        #         propositions.append(list_words)        
+        #return propositions            
             
             
                                   
