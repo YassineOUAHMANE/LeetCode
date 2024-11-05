8840fcb054dc64ad7fdb9a600188f1f58f3c65cb
YASSINE OUAHMANE <144154410+YassineOUAHMANE@users.noreply.github.com>
2024-11-05 10:39:01 +0100

    Time: 0 ms (100.00%), Space: 17.9 MB (5.70%) - LeetHub

--- 0206-reverse-linked-list/0206-reverse-linked-list.py
+++ 0206-reverse-linked-list/0206-reverse-linked-list.py
@@ -7,17 +7,86 @@
             
 class Solution:
     def reverseList(self, head: Optional[ListNode]) -> Optional[ListNode]:
-        if not head:
+            
+        # Recursive apprroach 
+        if not head or not head.next:
             return head
-        else:
-            prev = None
-            current = head
-            while current:
-                nxt = current.next
-                current.next = prev
-                prev = current
-                current = nxt
-            return prev
+        
+        reversed_list = self.reverseList(head.next)
+        
+        head.next.next = head
+        head.next = None
+        
+        return reversed_list
+        
+        
+        
+        
+            
+            
+            
+            
+            
+            
+            
+        
+        
+        
+        
+        
+        
+        
+        
+        #Iterative approach without creating a new linkedlist (no extra space)
+        #if not head:
+        #    return head
+        #else:
+        #    prev = None
+        #    current = head
+        #    while current:
+        #        nxt = current.next
+        #        current.next = prev
+        #        prev = current
+        #        current = nxt
+        #    return prev
+            
+            
+            
+            
+    
+            #Using a stack data structure (extra space) it won't work here memory excedeed
+            #if not head:
+            #    return head
+            #stack = []
+            #while head:
+            #    stack.append(head)
+            #    head = head.next
+            #dummy = ListNode(0)
+            #current = dummy
+            #while stack:
+            #    current.next = stack.pop()
+            #    current = current.next
+            #return dummy.next
+                
+            
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+       
                 
                
             
