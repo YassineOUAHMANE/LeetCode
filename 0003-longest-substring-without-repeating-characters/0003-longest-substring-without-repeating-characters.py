c4b067866fe7925487b4433ed351fd0bd703b2c0
YASSINE OUAHMANE <144154410+YassineOUAHMANE@users.noreply.github.com>
2024-11-09 10:29:49 +0100

    Time: 12 ms (89.07%), Space: 16.7 MB (28.20%) - LeetHub

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.py
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.py
@@ -1,20 +1,22 @@
 class Solution:
     def lengthOfLongestSubstring(self, s: str) -> int:
+            
         if not s:
             return 0
         if len(s) == 1:
             return 1
+        
+        element_visited = {}
         start = 0
-        longest = 0
-        visited_characters = {}
+        max_length = 0
         for i,el in enumerate(s):
-            if el in visited_characters:
-                #Check if the visited_character is in the current substring
-                if visited_characters[el] >= start: 
-                    start = visited_characters[el] + 1             
-            visited_characters[el] = i        
-            longest = max(longest,i-start+1)
-        return longest        
+            if el in element_visited and element_visited[el] >= start:
+                start = element_visited[el] + 1
+            element_visited[el] = i
+
+            max_length = max(max_length,i-start+1)
+
+        return max_length    
                 
                 
                 
@@ -50,19 +52,14 @@ class Solution:
         
         
         
-        #if not s:
-        #    return 0
-        #if len(s) == 1:
-        #    return 1
-        #
-        #element_visited = {}
-        #start = 0
-        #max_length = 0
-        #for i,el in enumerate(s):
-        #    if el in element_visited and element_visited[el] >= start:
-        #        start = element_visited[el] + 1
-        #    element_visited[el] = i
-#
-        #    max_length = max(max_length,i-start+1)
-#
-        #return max_length    
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
