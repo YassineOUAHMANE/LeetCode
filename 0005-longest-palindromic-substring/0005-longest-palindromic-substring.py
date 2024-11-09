cd7930bd290f4cc48d522ec4fdb3bc3db80bf7ef
YASSINE OUAHMANE <144154410+YassineOUAHMANE@users.noreply.github.com>
2024-11-09 12:36:11 +0100

    Time: 233 ms (82.65%), Space: 16.7 MB (31.28%) - LeetHub

---
+++ 0005-longest-palindromic-substring/0005-longest-palindromic-substring.py
@@ -0,0 +1,34 @@
+class Solution:
+    def longestPalindrome(self, s: str) -> str:
+        if not s:
+            return ""
+        if len(s) == 1:
+            return s
+        longest_palindrom = ""
+        palindrom_even = ""
+        palindrom_odd = ""
+        for i in range(len(s)):
+            #check for odd palindrom
+            low  = i 
+            high = i
+            while low>=0 and high<len(s) and s[high]==s[low]:
+                low-=1
+                high+=1
+            palindrom_even = s[low+1:high]   
+            #check for odd palindrom
+            low   = i - 1
+            high = i
+            while low>=0 and high<len(s) and s[high]==s[low] :
+                low-=1
+                high+=1
+            palindrom_odd = s[low+1:high]
+            #Update the longest palindrom
+            longest_palindrom = max(longest_palindrom,palindrom_odd,palindrom_even,key=len)
+        return longest_palindrom
+                    
+
+                    
+            
+                
+        
+        
