9910bf7e75a91b5b0851d4ff226c29dab41a59ea
YASSINE OUAHMANE <144154410+YassineOUAHMANE@users.noreply.github.com>
2024-11-09 18:40:27 +0100

    Time: 0 ms (100.00%), Space: 16.7 MB (73.70%) - LeetHub

--- 0404-sum-of-left-leaves/0404-sum-of-left-leaves.py
+++ 0404-sum-of-left-leaves/0404-sum-of-left-leaves.py
@@ -6,15 +6,56 @@
 #         self.right = right
 class Solution:
     def sumOfLeftLeaves(self, root: Optional[TreeNode]) -> int:
-            def mysum(root,is_left):
-                if not root:
-                    return 0
-                if  not root.left and not root.right and is_left:
-                    return root.val
-                left_sum = mysum(root.left,is_left=True) 
-                right_sum= mysum(root.right,is_left=False)
-                return left_sum + right_sum
-            return mysum(root,is_left=False)       
+           #ITERATIVE APPROACH
+            def is_leaf(root):
+                if root.left == None and root.right==None: return True
+                else: return False
+            if not root:
+                return 0
+            sum_value = 0 
+            queue = [root]
+            while queue:
+                node = queue.pop(0)
+                if node.left:
+                    queue.append(node.left)
+                    if is_leaf(node.left):
+                        sum_value += node.left.val
+                if node.right:
+                    queue.append(node.right)
+            return sum_value        
+                    
+                    
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+         #RECURSIVE APPROACH
+            #def mysum(root,is_left):
+            #    if not root:
+            #        return 0
+            #    if  not root.left and not root.right and is_left:
+            #        return root.val
+            #    left_sum = mysum(root.left,is_left=True) 
+            #    right_sum= mysum(root.right,is_left=False)
+            #    return left_sum + right_sum
+            #return mysum(root,is_left=False)       
             
             
         
