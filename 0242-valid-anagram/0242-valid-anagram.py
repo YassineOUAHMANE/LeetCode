4d6ca129c030b47de09fad28562a306c419e8bf0
YASSINE OUAHMANE <144154410+YassineOUAHMANE@users.noreply.github.com>
2024-11-09 14:36:03 +0100

    Time: 8 ms (81.83%), Space: 16.8 MB (73.74%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.py
+++ 0242-valid-anagram/0242-valid-anagram.py
@@ -1,4 +1,35 @@
 class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
-        return sorted(s) == sorted(t)
+        hash_map = {chr(i):0 for i in range(ord('a'),ord('z')+1)}
+        s = s.lower()
+        t = t.lower()
+        s = s.replace(" ","")
+        t = t.replace(" ","")
+        for el in s:
+            hash_map[el]+=1
+        for el in t:
+            hash_map[el]-=1
+        for value in hash_map.values():
+            if value!=0:
+                return False
+        return True
+            
+            
         
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        #return sorted(s) == sorted(t)    #O(nlog(n)) time complexity
