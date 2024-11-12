d921594f0f1464da1369e24a974288b7115f2de3
YASSINE OUAHMANE <144154410+YassineOUAHMANE@users.noreply.github.com>
2024-11-12 18:58:18 +0100

    Time: 3 ms (54.10%), Space: 18.3 MB (5.37%) - LeetHub

--- 0001-two-sum/0001-two-sum.py
+++ 0001-two-sum/0001-two-sum.py
@@ -1,14 +1,47 @@
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
-        hash_map = {}
-        result = []
-        for i,el in enumerate(nums):
-            if target - el in hash_map:
-                    result.append(hash_map[target-el])
-                    result.append(i)   
+        left = 0 
+        right = len(nums) - 1
+        nums_with_indices = [(num, i) for i, num in enumerate(nums)]
+        nums_with_indices.sort()  # Sort by the values
+        while left < right:
+            if nums_with_indices[left][0] + nums_with_indices[right][0] < target:
+                left+=1
+            elif nums_with_indices[left][0] + nums_with_indices[right][0] > target:
+                right-=1
+             
             else:
-                hash_map[el] = i
-        return result        
+                return [nums_with_indices[left][1],nums_with_indices[right][1]]
+           
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        #hash_map = {}
+        #result = []
+        #for i,el in enumerate(nums):
+        #    if target - el in hash_map:
+        #            result.append(hash_map[target-el])
+        #            result.append(i)   
+        #    else:
+        #        hash_map[el] = i
+        #return result        
                 
                 
                 
